07d748881156c7ee0503a662ae3136e1e3968758
Ian Huang <ianmhuang@gmail.com>
2025-06-15 21:07:47 +0800

    [Chap 5] Structure of a C++ program 1. Create 3 exercises for cout/cin operations.

---
+++ chap_05/exercise_2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int x {3};
+
+    std::cout << "Sally has " << x << " dogs.";
+    return 0;
+}
---
+++ chap_05/exercise_3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int m {};
+    int d {};
+    int y {};
+
+    std::cout << "Please input 3 integers: ";
+    std::cin >> m >> d >> y;
+    std::cout << "m: " << m << ". d: " << d << ". y: " << y << "." << std::endl;
+    
+    return 0;
+}
